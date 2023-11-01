b0b4ceecaec49923e517b30961cb74f7f4c3ce23
Ayaan Adil <z5213315@ad.unsw.edu.au>
2023-11-01 20:11:39 +1100

    pacific

--- graphs/pacificAtlantic.py
+++ graphs/pacificAtlantic.py
@@ -1,96 +1,35 @@
-def pacificAtlantic(heights):
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac, atl = set(), set()
 
-    pacific = [[None] * len(heights[0])] * len(heights)
-    atlantic = [[None] * len(heights[0])] * len(heights)
-    visited = []
+        def dfs(r, c, visit, prevHeight):
+            if (
+                (r, c) in visit
+                or r < 0
+                or c < 0
+                or r == ROWS
+                or c == COLS
+                or heights[r][c] < prevHeight
+            ):
+                return
+            visit.add((r, c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
 
-    def dfs(i, j):
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
 
-        print ("DFS   " + str(i) + "  " + str(j))
-        print(pacific)
-        print(atlantic)     
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
 
-        if (i < 0 or i >= len(heights) or j < 0 or j >= len(heights[0])):
-            print("Invalid Indices")
-            return (False, False)
-
-        if ((i,j) in visited):
-            print("This was visited: ")
-            print("i: " + str(i) + " j: " + str(j) + "  " + str(pacific[i][j]))
-            print("i: " + str(i) + " j: " + str(j) + "  " + str(atlantic[i][j]))
-            return (pacific[i], atlantic[j])
-        else:
-            
-            visited.append((i,j))
-
-        print("VISITED: ")
-        print(visited)
-
-
-        if (i == 0 or j == 0):
-            print("====")
-            print(pacific[i])
-            print(pacific[i][j])
-            pacific[i][j] = True
-            print(pacific)
-            print("====")
-
-        if (i == (len(heights) - 1) or j == (len(heights[0]) - 1)):
-            atlantic[i][j] = True
-
-
-        if (pacific[i][j] == None or atlantic[i][j] == None):
-            
-            (a, b) = dfs(i+1, j)
-            (c, d) = dfs(i-1, j)
-            (e, f) = dfs(i, j+1)
-            (g, h) = dfs(i, j-1)
-
-
-            if (((a == True) and (heights[i][j] >= heights[i+1][j])) or 
-                ((c == True) and (heights[i][j] >= heights[i-1][j])) or 
-                ((e == True) and (heights[i][j] >= heights[i][j+1])) or 
-                ((g == True) and (heights[i][j] >= heights[i][j-1]))):
-
-                pacific[i][j] = True
-                
-            else:
-                pacific[i][j] = False
-
-            
-
-            if (((b == True) and (heights[i][j] >= heights[i+1][j])) or 
-                ((d == True) and (heights[i][j] >= heights[i-1][j])) or 
-                ((f == True) and (heights[i][j] >= heights[i][j+1])) or 
-                ((h == True) and (heights[i][j] >= heights[i][j-1]))):
-                atlantic[i][j] = True
-
-            else:
-                pacific[i][j] = False
-
-
-        print("i: " + str(i) + " j: " + str(j) + "  " + str(pacific[i][j]))
-        print("i: " + str(i) + " j: " + str(j) + "  " + str(atlantic[i][j]))
-
-        return (pacific[i][j],atlantic[i][j])
-
-    
-    for i in range(0, len(heights)):
-        for j in range(0, len(heights[0])):
-            if (pacific[i][j] == None and atlantic[i][j] == None):
-                dfs(i, j)
-
-    print(pacific)
-    print(atlantic)              
-
-    result = []
-    for i in range(0, len(heights)):
-        for j in range(0, len(heights[0])):
-            if (pacific[i][j] == True and atlantic[i][j] == True):
-                result.append([i, j])
-
-
-    return result
-
-if __name__ == "__main__":
-    pacificAtlantic([[1,2],[3,2]])
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        return res
